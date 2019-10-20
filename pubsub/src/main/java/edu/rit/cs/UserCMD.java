553530cef3fc8f6c3dc86074353339bc60d37aa7
jefftaglic <jmtaglic98@gmail.com>
2019-10-20 14:59:35 -0400

    User CLI for account creation, signin

--- pubsub/src/main/java/edu/rit/cs/UserCMD.java
+++ pubsub/src/main/java/edu/rit/cs/UserCMD.java
@@ -9,7 +9,7 @@ import java.util.*;
 
 public class UserCMD {
 
-    private static enum cmdBegin() {
+    private static User cmdBegin() {
         Scanner initial = new Scanner(System.in);
 
         System.out.println(
@@ -18,11 +18,105 @@ public class UserCMD {
                 "=================================\n\n");
 
         System.out.println("Pick an option:\n" +
-                            "Create User(\"create\"" + "\t\t")
+                            "Create User(\"create\"" + "\t\t" +
+                            "Sign In(\"signin\"");
+
+        while(true) {
+            String checkOpt = initial.nextLine(); // Read user's decision
+            if (checkOpt.equals("create")) {
+                return usrCreate();
+            } else if (checkOpt.equals("signin")) {
+                return usrSignin();
+            } else {
+                System.out.println("Does not match either of the commands.\n");
+                System.out.println("Please enter \"create\" or \"signin\".\n");
+            }
+        }
+    }
+
+    private static User usrCreate(){
+        // send msg saying new user
+
+        Scanner create = new Scanner(System.in);
+
+        String user = "";
+        String pass = "";
+        String roleStr = "";
+        User.pubOrSub role = null;
+        boolean uniqueId = false;
+        while(!(uniqueId)) {
+            System.out.println("\nPlease enter in a new username: ");
+            user = create.nextLine();
+            // TODO check username here, make equal to uniqueId
+            if (!(uniqueId)){
+                System.out.println("Username is taken already.\n");
+            }
+        }
+
+        boolean passChk = true;
+        while( passChk ) {
+            System.out.println("Please enter a new password: ");
+            pass = create.nextLine();
+            System.out.println("Re-enter password: ");
+            passChk = pass.equals(create.nextLine());
+            if (passChk){
+                System.out.println("Account created.\n");
+                passChk = false;
+            } else {
+                System.out.println("Passwords do not match.\n");
+                passChk = true;
+            }
+        }
+
+        System.out.println("Are you a subscriber(\"sub\") or a publisher(\"pub\"?");
+        while(role == null) {
+            System.out.println("Please clarify here: ");
+            roleStr = create.nextLine();
+            if (roleStr.equals("sub")){
+                role = User.pubOrSub.SUB;
+            } else if (roleStr.equals("pub")){
+                role = User.pubOrSub.PUB;
+            } else {
+                System.out.println("Does not match either of the commands.\n");
+                System.out.println("Please enter \"sub\" or \"pub\".\n");
+            }
+        }
+        return new User(role, user, pass);
+    }
+
+    public static User usrSignin() {
+        Scanner user_input = new Scanner(System.in);
+        while (true){
+            System.out.println("Enter username: ");
+            String user = user_input.nextLine();
+            String pass = "";
+            if (/* TODO check if username exists */) {
+                while (true) {
+                    System.out.println("Enter password: ");
+                    pass = user_input.nextLine();
+                    int pass_tries = 0;
+                    if (/* TODO check to see if password is correct*/) {
+                        User node_user = null;
+                        // TODO get user from eventmanager hashmap
+                        return node_user;
+                    } else {
+                        if (pass_tries > 3) {
+                            System.out.println("Too many attempts. Program terminating.");
+                            System.exit(1);
+                        } else {
+                            System.out.println("Password does not match, please try again.");
+                            pass_tries++;
+                        }
+                    }
+                }
+            } else {
+                System.out.println("Username does not exist. Please try again. \n ");
+            }
+        }
     }
 
     private static void startCMD() {
-        cmdBegin();
+        User currUser = cmdBegin();
     }
 
     public static void main(String[] args) {
