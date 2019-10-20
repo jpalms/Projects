244ceac7dc9aac0618952d23661b3f952fa01075
jefftaglic <jmtaglic98@gmail.com>
2019-10-20 15:29:37 -0400

    Event has constructor, get/set methods

--- pubsub/src/main/java/edu/rit/cs/Event.java
+++ pubsub/src/main/java/edu/rit/cs/Event.java
@@ -5,4 +5,43 @@ public class Event {
 	private Topic topic;
 	private String title;
 	private String content;
+
+    public Event(int id, Topic topic, String title, String content) {
+        this.id = id;
+        this.topic = topic;
+        this.title = title;
+        this.content = content;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Topic getTopic() {
+        return topic;
+    }
+
+    public void setTopic(Topic topic) {
+        this.topic = topic;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
 }
