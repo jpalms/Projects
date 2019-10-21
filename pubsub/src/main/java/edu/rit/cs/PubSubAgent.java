3922c4b796d290d773fa5ea59369f1cc263feaf3
Justin <jpalms2021@gmail.com>
2019-10-20 21:02:07 -0400

    Updated PubSubAgent to reflect how it was used as an abstract class

--- pubsub/src/main/java/edu/rit/cs/PubSubAgent.java
+++ pubsub/src/main/java/edu/rit/cs/PubSubAgent.java
@@ -1,16 +1,10 @@
 package edu.rit.cs;
 
 
-public class PubSubAgent implements Publisher, Subscriber{
-
-
-	public static Topic topicExist(String check_string){
-		// TODO - FIGURE THIS OUT
-	}
+public abstract class PubSubAgent implements Publisher, Subscriber{
 
 	@Override
 	public void subscribe(Topic topic) {
-		// TODO Auto-generated method stub
 
 		/*
 		topic.
@@ -20,38 +14,30 @@ public class PubSubAgent implements Publisher, Subscriber{
 
 	@Override
 	public void subscribe(String keyword) {
-		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void unsubscribe(Topic topic) {
-		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
 	public void unsubscribe() {
-		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
 	public void listSubscribedTopics() {
-		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void publish(Event event) {
-		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void advertise(Topic newTopic) {
-		// TODO Auto-generated method stub
-		
+
 	}
 
 
