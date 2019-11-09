14b17c638bc1afc4730f8a31bfd2dc337097f368
jefftaglic <jmtaglic98@gmail.com>
2019-11-09 17:00:56 -0500

    altered some code in serverCLI, still doesn't work yet

--- chord/src/main/java/edu/rit/cs/view/ServerCLI.java
+++ chord/src/main/java/edu/rit/cs/view/ServerCLI.java
@@ -1,9 +1,11 @@
 package edu.rit.cs.view;
 
+/*
 import edu.rit.cs.controller.EventManager;
 import edu.rit.cs.controller.NotifyAll;
 import edu.rit.cs.model.Topic;
 import edu.rit.cs.model.User;
+*/
 
 import java.util.HashMap;
 import java.util.Scanner;
@@ -11,86 +13,61 @@ import java.util.Scanner;
 public class ServerCLI {
 
     /**
-     * Function to gracefully terminate the node.
-     * Takes an EventManager and a handler
+     * Function to gracefully terminate the server node.
+     * Takes a handler thread (with the connections to the peer nodes of the chords
      * No return, but calls System.exit with status (1).
      *
      * @param n - Handler passed in, used in stopService
      */
-    public static void emTerminate(NotifyAll n){
-        n.turnOff();
-        System.out.println("EventManager has been terminated.");
+    public static void serverTerminate( /* TODO put something here */){
+        something.turnOff();
+        System.out.println("Server has been terminated.");
         System.exit(1);
     }
 
     /**
-     * Function to show all subscribers in the EventManager.
-     * Takes an EventManager as a parameter.
+     * Function to show all chord nodes in the Server.
+     * Takes an Server as a parameter.
      * No return, but prints the list of subscribers to console.
      */
-    public static void showAllsubs(EventManager em){
-        em.showAllSubs();
+    public static void showChord(Server sv){
+        sv.showAllNodes();
     }
 
     /**
-     * Function to show all subscribers to a given topic, based on user input.
-     * Takes an EventManager as a parameter.
-     * Within the function, takes user input and either
-     *      -prints out a list of users that have subscribed to the given topic
-     *      -prints a pseudo-error message, returns you to the main prompt
+     * Function to start up the Server's CLI.
+     * Takes an Server and a Handler, to be passed later on.
+     * No return value, but responds based on user input.
      *
-     * @param em - used to get the topic HashMap
+     * @param sv - Server currently running on the node
+     * @param h - Handler to be passed on later
      */
-    public static void showSubs(EventManager em){
-        Scanner show = new Scanner(System.in);
-        System.out.println("Please input the topic that you want to show subscribers for: ");
-        String show_str = show.nextLine();
-        HashMap<String, Topic> t_list = em.getTopicMap();
-        Topic topic = t_list.get(show_str);
-        if (topic != null){
-            for(User user:topic.getSubs().values()){
-                System.out.println(user);
-            }
-        } else {
-            System.out.println("Input does not match available options.\n" +
-                    "Returning to command list...\n");
-        }
-    }
+    public void startCLI(Server sv, Handler h){
+        System.out.println("============SERVER============\n");
 
-    /**
-     * Function to start up the EventManager's CLI.
-     * Takes an EventManager and a Handler, to be passed later on.
-     * No return, but responds based on user input.
-     *
-     * @param em - EventHandler currently running on the node
-     */
-    public void startCLI(EventManager em, NotifyAll n){
-        System.out.println("============EVENTMANAGER============\n");
-
-        Scanner em_input = new Scanner(System.in);
+        Scanner sv_input = new Scanner(System.in);
         boolean exit_flag = true;
         do{
             System.out.println("Commands available: \n" +
                     "Terminate (\"t\") \t" +
-                    "Show All Subscribers (\"a\") \t" +
-                    "Show Subscriber(s) for a Topic (\"s\") \t"
+                    "Show All Nodes (\"a\") \t" // +
+                    // "Show Subscriber(s) for a Topic (\"s\") \t"
             );
-            String command = em_input.nextLine();
+            String command = sv_input.nextLine();
             switch(command){
                 case "t":
-                    emTerminate(n);
+                    serverTerminate();
                     exit_flag = false;
                     break;
                 case "a":
-                    showAllsubs(em);
+                    showChord(sv);
                     break;
-                case "s":
-                    showSubs(em);
-                    break;
+                //case "s":
+                //    showSubs(em);
+                //    break;
                 default:
-                    System.out.println("Not an available command for subscribers.\n");
+                    System.out.println("Not an available command for servers.\n");
             }
         } while (exit_flag);
     }
-
 }
