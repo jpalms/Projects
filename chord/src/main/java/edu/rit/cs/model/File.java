c25889f3fc32a08f1d3fae16987abb6a4b331221
Justin <jpalms2021@gmail.com>
2019-11-17 15:59:23 -0500

    Creating File Class to use instead of abstract java.io.File class for chord File distribution

--- chord/src/main/java/edu/rit/cs/model/File.java
+++ chord/src/main/java/edu/rit/cs/model/File.java
@@ -3,7 +3,38 @@ package edu.rit.cs.model;
 public class File {
 
     private String path;
+    private String fileContent, fileName;
+
     public File(String filePath){
         this.path = filePath;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setFileContent(String fileContent) {
+        this.fileContent = fileContent;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(!(obj instanceof File))
+            return false;
+        return this.path.equals(((File) obj).getPath());
+    }
+
+    @Override
+    public String toString() {
+        return "File: " + fileName;
+    }
+
+    @Override
+    public int hashCode() {
+        return fileName.hashCode();
     }
 }
