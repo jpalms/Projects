3c42f8a5821aab689cb9bb8d6767adf699dc5df0
Justin <jpalms2021@gmail.com>
2019-12-06 20:34:28 -0500

    Output is now written to a file and comments have been added

--- us_census_diversity_index/src/main/java/edu/rit/cs/USCB_Spark.java
+++ us_census_diversity_index/src/main/java/edu/rit/cs/USCB_Spark.java
@@ -1,23 +1,17 @@
 package edu.rit.cs;
 
 import edu.rit.cs.model.Config;
-import edu.rit.cs.model.Partition;
 import edu.rit.cs.model.USCBPopulationStat;
+
 import org.apache.spark.SparkConf;
-import org.apache.spark.SparkContext;
 import org.apache.spark.api.java.JavaRDD;
-import org.apache.spark.api.java.function.ForeachFunction;
-import org.apache.spark.api.java.function.VoidFunction;
-import org.apache.spark.rdd.RDD;
-import org.apache.spark.sql.functions;
+
 import org.apache.spark.sql.*;
-import scala.Function1;
 
 import java.io.File;
-import java.sql.*;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Iterator;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 /**
  * U.S. Census Diversity Index
@@ -25,9 +19,14 @@ import java.util.Iterator;
  */
 public class USCB_Spark
 {
-    public static final String OutputDirectory = "dataset/USCB-outputs";
+    public static final String OutputDirectory = "us_census_diversity_index/dataset/USCB-outputs";
     public static final String DatasetFile = "dataset/USCB.csv";
 
+    /**
+     *
+     * @param directoryToBeDeleted - directory which gets deleted
+     * @return - true if deleted, false is directory doesn't exist
+     */
     public static boolean deleteDirectory(File directoryToBeDeleted) {
         File[] allContents = directoryToBeDeleted.listFiles();
         if (allContents != null)
@@ -36,9 +35,12 @@ public class USCB_Spark
         return directoryToBeDeleted.delete();
     }
 
+    /**
+     * Manipulates the datables to determine the Diversity Index for all ages and year in each state, county
+     * @param spark - spark session
+     */
     public static void uscb(SparkSession spark) {
         // parse dataset file
-        int year = 2010;
         Dataset ds = spark.read()
                 .option("header", "true")
                 .option("sep", ",")
@@ -49,20 +51,21 @@ public class USCB_Spark
         Encoder<USCBPopulationStat> uscbEncoder = Encoders.bean(USCBPopulationStat.class);
         Dataset<USCBPopulationStat> ds1 = ds.as(uscbEncoder);
         // show initial table after import
+        System.out.println("Show ds1");
         ds1.show();
 
         // filter and sum data across multiple years for all year groups
-        Dataset ds2 = ds1.filter("AGEGRP = " + Config.AGEGRP)
-                .select("STNAME", "CTYNAME",
+        Dataset ds2 = ds1.filter(Config.AGEGRP)
+                .select(Config.STATE, Config.CITY,
                 "WA_MALE", "WA_FEMALE", "BA_MALE", "BA_FEMALE", "IA_MALE", "IA_FEMALE", "AA_MALE", "AA_FEMALE", "NA_MALE", "NA_FEMALE", "TOM_MALE", "TOM_FEMALE")
                 .groupBy("STNAME", "CTYNAME")
-                .sum().orderBy("STNAME", "CTYNAME");
+                .sum().orderBy(Config.STATE, Config.CITY);
 
-        // You need to complete the rest
-
+        // show table of all ages
         System.out.println("Show ds2");
         ds2.show();
 
+        // combine gender
         Dataset ds3 = ds2
                         .withColumn(Config.WA, ds2.col(Config.WA_MALE).plus(ds2.col(Config.WA_FEMALE))).drop(Config.WA_MALE).drop(Config.WA_FEMALE)
                         .withColumn(Config.BA, ds2.col(Config.BA_MALE).plus(ds2.col(Config.BA_FEMALE))).drop(Config.BA_MALE).drop(Config.BA_FEMALE)
@@ -71,16 +74,17 @@ public class USCB_Spark
                         .withColumn(Config.NA, ds2.col(Config.NA_MALE).plus(ds2.col(Config.NA_FEMALE))).drop(Config.NA_MALE).drop(Config.NA_FEMALE)
                         .withColumn(Config.TOM, ds2.col(Config.TOM_MALE).plus(ds2.col(Config.TOM_FEMALE))).drop(Config.TOM_MALE).drop(Config.TOM_FEMALE);
 
+        // show table of all ages and race's genders are combined
         System.out.println("Show ds3");
         ds3.show();
 
+        // calculate total population
         ds3 = ds3.withColumn(Config.TOTAL_POP, ds3.col(Config.WA).plus(ds3.col(Config.BA)).plus(ds3.col(Config.IA).plus(ds3.col(Config.AA).plus(ds3.col(Config.NA).plus(ds3.col(Config.TOM))))));
 
-        System.out.println("Show ds3");
-        ds3.show();
-
+        // List of Column names
         String [] arr = ds3.columns();
 
+        // calculates the summation in the Diversity Index formula
         for (int i = 0; i < Config.NUM_RACES; i++) {
             String col = arr[i + 2];
             if(i == 0)
@@ -89,29 +93,47 @@ public class USCB_Spark
                 ds3 = ds3.withColumn(Config.SIGMA, ds3.col(Config.SIGMA).plus(ds3.col(col).multiply(ds3.col(Config.TOTAL_POP).minus(ds3.col(col))))).drop(col);
         }
 
+        // divides the summation by total population squared to get the Diversity Index
         ds3 = ds3.withColumn(Config.DIV, ds3.col(Config.SIGMA).divide(ds3.col(Config.TOTAL_POP).multiply(ds3.col(Config.TOTAL_POP)))).drop(Config.TOTAL_POP, Config.SIGMA);
+
+        // show diversity index
         ds3.show();
 
+        // repartition with minimal shuffling
+        JavaRDD javaRDD = ds3.toJavaRDD().coalesce(1, true);
 
+        // empty directory
+        File file = new File(OutputDirectory);
+        deleteDirectory(file);
 
-        //Dataset
+        // save table results in the empty directory
+        javaRDD.saveAsTextFile(OutputDirectory);
 
+        // rename the output file
+        try {
+            Files.move(Paths.get(OutputDirectory + "/part-00000"),
+                    Paths.get(OutputDirectory + "/USCB-outputs.txt"));
+        }catch(IOException e){
+            System.err.println("File Not Found");
+        }
     }
 
     public static void main( String[] args )
     {
-       // fill your code
+       // sets path to hadoop
+        System.setProperty("hadoop.home.dir", "C:\\winutils\\");
 
+        // set ups up data to be separated into 4 partitions
         SparkConf sparkConf = new SparkConf();
         sparkConf.set("spark.master", "local[4]");
         sparkConf.setAppName("Census Report");
 
+        // sets up spark
         SparkSession sparkSession = SparkSession.builder().config(sparkConf).getOrCreate();
-
-        System.out.println("get ready");
 
         uscb(sparkSession);
 
+        // closes spark session
         sparkSession.close();
     }
 }
--- us_census_diversity_index/src/main/java/edu/rit/cs/model/Config.java
+++ us_census_diversity_index/src/main/java/edu/rit/cs/model/Config.java
@@ -1,26 +1,11 @@
 package edu.rit.cs.model;
 
-import org.apache.spark.sql.Dataset;
-import org.apache.spark.sql.Row;
-import scala.collection.Seq;
-
-import java.util.ArrayList;
-import java.util.Iterator;
-
+/**
+ * Reference variables
+ */
 public class Config {
 
-    public final String YEAR_1 = "sum(April 1, 2010 census population";
-    public final String YEAR_2 = "sum(April 1, 2010 population estimates base";
-    public final String YEAR_3 = "July 1, 2010 population estimate";
-    public final String YEAR_4 = "July 1, 2011 population estimate";
-    public final String YEAR_5 = "July 1, 2012 population estimate";
-    public final String YEAR_6 = "July 1, 2013 population estimate";
-    public final String YEAR_7 = "July 1, 2014 population estimate";
-    public final String YEAR_8 = "July 1, 2015 population estimate";
-    public final String YEAR_9 = "July 1, 2016 population estimate";
-    public final String YEAR_10 = "July 1, 2017 population estimate";
-
-    public static final int AGEGRP = 0;
+    public static final String AGEGRP = "AGEGRP = 0";
     public static final int NUM_RACES = 6;
 
     public static final String STATE = "STNAME";
@@ -54,68 +39,4 @@ public class Config {
 
     public static final String DIV = "DIV";
 
-    public String getYear(int year){
-        switch (year){
-            case (1): {
-                return YEAR_1;
-            }
-            case (2): {
-                return YEAR_2;
-            }
-            case (3):{
-                return YEAR_3;
-            }
-            case (4):{
-                return YEAR_4;
-            }
-            case (5):{
-                return YEAR_5;
-            }
-            case (6): {
-                return YEAR_6;
-            }
-            case (7): {
-                return YEAR_7;
-            }
-            case (8):{
-                return YEAR_8;
-            }
-            case (9):{
-                return YEAR_9;
-            }
-            case (10):{
-                return YEAR_10;
-            }
-            default:
-                return "DNE";
-        }
-    }
-
-    public int getAgeGroup(){
-        return AGEGRP;
-    }
-
-    public static String calcDivIndex(Iterator rows) {
-
-        String answer = "";
-        while (rows.hasNext()) {
-            Row row = (Row)rows.next();
-            int total = 0;
-            double result = 0;
-            double[] arr = new double[NUM_RACES];
-
-            for (int i = 0; i < NUM_RACES; i++) {
-                arr[i] = row.getLong(i * 2 + 2) + row.getLong(i * 2 + 3);
-                total += arr[i];
-            }
-
-            for (int i = 0; i < arr.length; i++) {
-                result += arr[i] * (total - arr[i]);
-            }
-            result = result / Math.pow(total, 2);
-
-            answer += ("[" + row.getString(0) + "," + row.getString(1) + "," + result + "]\n");
-        }
-        return answer;
-    }
 }
